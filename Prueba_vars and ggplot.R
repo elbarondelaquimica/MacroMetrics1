50eb852abe174991ab492e99de145b0d52573365
franco-nunez <73083742+franco-nunez@users.noreply.github.com>
2020-10-29 17:05:24 -0300

    Create Prueba_vars and ggplot.R

---
+++ Prueba_vars and ggplot.R
@@ -0,0 +1,35 @@
+# Conversión para ggplot2
+
+#Paquetes necesarios
+library(tidyverse)
+library(reshape2)
+library("tibble")
+library("gridExtra")
+library("dplyr")
+library("Lock5Data")
+library("ggthemes")
+library("fun")
+library("zoo")
+library("corrplot")
+library("maps")
+library("mapproj")
+
+#Genero la función
+
+grafico <- function(SVAR.ERPT.boot, H_ERPT){
+  t = c(0:120)
+  datos <- data.frame(t, SVAR.ERPT.boot$lb, SVAR.ERPT.boot$pe, SVAR.ERPT.boot$ub)
+  colnames(datos) <- c("t", "lb", "pe", "ub")
+  grafico <- ggplot(data=datos,aes(x= t, y=pe))
+  grafico <- grafico + geom_ribbon(aes(x=t, ymax=ub, ymin=lb), fill="olivedrab3", alpha=.25) 
+  grafico <-  grafico +geom_line(aes(y = ub), colour = 'darkolivegreen3') 
+  grafico <-  grafico + geom_line(aes(y = lb), colour = 'darkolivegreen3')
+  grafico <- grafico + geom_line(colour = "darkolivegreen4", size =1.15)
+  grafico <- grafico + labs(x = "Mes", y =" ", title = "Exchange rate pass through", subtitle = "En porcentaje")
+  grafico <- grafico + theme_minimal()
+  grafico <- grafico + theme(axis.ticks = element_blank())
+  grafico
+}
+
+#Ejemplo con ej ejericio 12 y 120 períodos adelante:  
+grafico(SVAR.ERPT.boot_12, 120)  
