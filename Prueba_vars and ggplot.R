11edf8f9860e98498970f2ec04c486a27db6c7e3
franco-nunez <73083742+franco-nunez@users.noreply.github.com>
2020-10-28 22:58:27 -0300

    Update Prueba_vars and ggplot.R

--- Prueba_vars and ggplot.R
+++ Prueba_vars and ggplot.R
@@ -1,20 +1,25 @@
 #Prueba VAR and ggplot
 
-#Función para convertir a data frame lo que sale de la función ERPT:
+#FunciÃ³n para convertir a data frame lo que sale de la funciÃ³n ERPT:
 convertir_a_df <- function(SVAR.erpt.boot, H_ERPT){
   t = c(0:H_ERPT)
   data.frame(t, SVAR.erpt.boot$lb, SVAR.erpt.boot$pe, SVAR.erpt.boot$ub)
 }
 
-SVAR.erpt.boot$lb <- as.numeric(SVAR.erpt.boot$lb)
-#Acá tomo uno estimado para ver si funciona
+
+#AcÃ¡ tomo uno estimado para ver si funciona
 prueba <- convertir_a_df(SVAR.ERPT.boot_12, H_ERPT = 120)
 prueba
 
-#Parece que funciona, así que ahora hay que graficar. Antes, hay que cambiar el formato del dataframe:
-# TBC
+#Parece que funciona, asÃ­ que ahora hay que graficar. Antes, hay que cambiar el formato del dataframe:
+library(reshape2)
+prueba_melt <- melt(prueba,id="t")
 
-#Ahora sí, el dichoso gráfico:
+#Ahora sÃ­, el dichoso grÃ¡fico:
 library(tidyverse)
-grafico1 <- ggplot(data=prueba, aes_string(prueba$t, prueba$lb, prueba$pe, prueba$up)) +
+grafico1 <- ggplot(data=prueba_melt, aes_string(prueba$t, prueba$lb, prueba$pe, prueba$up)) +
   geom_line(aes(y = prueba$lb), colour = 'lightblue2')
+
+grafico1 <- ggplot(data=meltdf,aes(x= t, y=value,colour=variable,group=variable)) +
+  geom_line()
+grafico1
